4c9ff7ef8c6fc034b4deddd59b730ead79d2c935
Nate12503 <nathanshussey@gmail.com>
2020-11-11 21:11:35 -0500

    Created State Class

---
+++ Supervisor/Assets/Scripts/Productivity FSM/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State : MonoBehaviour
+{
+    protected virtual void OnEnter() 
+    {
+        StartCoroutine("OnCurrentState");
+    }
+
+    protected void OnExit() 
+    {
+        StopAllCoroutines();
+    }
+
+    protected abstract IEnumerable OnCurrentState();
+}
