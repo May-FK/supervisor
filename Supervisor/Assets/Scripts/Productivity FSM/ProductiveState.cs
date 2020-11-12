ef51217f8f7c23b24db0402b0243f11656e5b321
Nate12503 <nathanshussey@gmail.com>
2020-11-12 01:02:15 -0500

    states!

---
+++ Supervisor/Assets/Scripts/Productivity FSM/ProductiveState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductiveState : State
+{
+    protected override IEnumerable OnCurrentState()
+    {
+        yield return null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Supervisor/Assets/Scripts/Productivity FSM/ProductivityFSMController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductivityFSMController : MonoBehaviour
+{
+    public enum ProductivityState
+    {
+        Productive,
+        Distracted,
+        Social
+    }
+
+    private ProductivityState CurrentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentState = ProductivityState.Productive;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RunCurrentState()
+    {
+        switch (CurrentState)
+        {
+            case ProductivityState.Productive:
+                break;
+
+            case ProductivityState.Distracted:
+                break;
+
+            case ProductivityState.Social:
+                break;
+        }
+    }
+}
