8223a6c1c988fa0ac06bcec650cc21e1570fb218
Nate12503 <nathanshussey@gmail.com>
2020-11-18 20:06:32 -0500

    Productivity State Machine

---
+++ Supervisor/Assets/Scripts/ProductivityStateMachine.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductivityStateMachine : MonoBehaviour
+{
+    public enum ProductivityState
+    {
+        Productive,
+        Distracted,
+        Social
+    }
+
+    private MoodStateMachine MoodState;
+    private ProductivityState CurrentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RunCurrentState();
+    }
+
+    public IEnumerator ProductiveState()
+    {
+        Debug.Log("Employee is being Productive.");
+        //gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+        yield return null;
+    }
+
+    //Employee turns cyan if in cheerful state
+    public IEnumerator DistractedState()
+    {
+        Debug.Log("Employee is distracted");
+       // gameObject.GetComponent<MeshRenderer>().material.color = Color.cyan;
+        yield return null;
+    }
+
+    //Employee turns red if in annoyed state
+    public IEnumerator SocialState()
+    {
+        Debug.Log("Employee is being social.");
+       // gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        yield return null;
+    }
+
+    private void RunCurrentState()
+    {
+        switch (CurrentState)
+        {
+            case ProductivityState.Productive:
+                StartCoroutine("ProductiveState");
+                break;
+
+            case ProductivityState.Distracted:
+                StartCoroutine("DistractedState");
+                break;
+
+            case ProductivityState.Social:
+                StartCoroutine("SocialState");
+                break;
+        }
+    }
+}
