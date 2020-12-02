384837bd3b7d344a47a0fafa78bd8883c15a7ab8
jenfield99 <64561971+jenfield99@users.noreply.github.com>
2020-12-01 23:16:11 -0500

    score stuff is implemented, numbers are still wonky tho
    maybe take a look? the score increases super high and decreases super low, both super fast. i'm trying to get it to increment once and i feel like i'm missing one basic thing. dunno what yet but also barely awake rn so

---
+++ Supervisor/Assets/Scripts/Score.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private double startScore = 0;
+    private double currentScore = 0;
+    public GameObject[] employees;
+    [SerializeField]
+    Text scoreDisplay;
+    private Color employeeColor;
+    
+
+    void Start()
+    {
+        scoreDisplay.text = startScore.ToString("Score: " + startScore);
+    }
+
+    void Update()
+    {   
+        scoreDisplay.text = currentScore.ToString("Score: " + currentScore);
+        EmployeeProductivity();
+    }
+
+    void EmployeeProductivity()
+    {
+        foreach (GameObject employee in employees)
+        {
+            employeeColor = employee.gameObject.GetComponent<MeshRenderer>().material.color;
+
+            if (employeeColor == Color.cyan)
+            {
+                Debug.Log("Score is increasing based on productivity.");
+                currentScore = currentScore + 1;
+                Debug.Log(currentScore);
+            }
+
+            else if (employeeColor == Color.red)
+            {
+                Debug.Log("Score is decreasing based on productivity.");
+                currentScore = currentScore - 1;
+                Debug.Log(currentScore);
+            }
+        }
+    }
+}
