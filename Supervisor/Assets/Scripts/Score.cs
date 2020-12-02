98d6f73210ca4107b951e86f9bbb38a3efd49e4e
jenfield99 <64561971+jenfield99@users.noreply.github.com>
2020-12-01 23:29:37 -0500

    fixed score stuff(mostly anyway)

--- Supervisor/Assets/Scripts/Score.cs
+++ Supervisor/Assets/Scripts/Score.cs
@@ -5,9 +5,14 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    private double startScore = 0;
-    private double currentScore = 0;
+
+    //variables n stuff, check the inspector
+    private float startScore = 0f;
+    private float currentScore = 0f;
+
+    //drag & drop employee + supervisor game objects in the inspector for this to work
     public GameObject[] employees;
+    public GameObject supervisor;
     [SerializeField]
     Text scoreDisplay;
     private Color employeeColor;
@@ -15,33 +20,49 @@ public class Score : MonoBehaviour
 
     void Start()
     {
-        scoreDisplay.text = startScore.ToString("Score: " + startScore);
+        scoreDisplay.text = startScore.ToString("Score: ");
     }
 
     void Update()
     {   
-        scoreDisplay.text = currentScore.ToString("Score: " + currentScore);
+        //rounds the float to an integer so the score can only be whole numbers
+        scoreDisplay.text = currentScore.ToString("Score: " + Mathf.RoundToInt(currentScore));
+        
         EmployeeProductivity();
+        
+        //stops the score from going below 0
+        if (currentScore <= 0f)
+        {
+            currentScore = 0f;
+        }
     }
 
     void EmployeeProductivity()
     {
         foreach (GameObject employee in employees)
         {
+
+            //gets the employee color
             employeeColor = employee.gameObject.GetComponent<MeshRenderer>().material.color;
 
-            if (employeeColor == Color.cyan)
+            //checks if the supervisor is within distance to add to score
+            if (Vector3.Distance(supervisor.transform.position, employee.transform.position) < 5)
             {
-                Debug.Log("Score is increasing based on productivity.");
-                currentScore = currentScore + 1;
-                Debug.Log(currentScore);
-            }
-
-            else if (employeeColor == Color.red)
-            {
-                Debug.Log("Score is decreasing based on productivity.");
-                currentScore = currentScore - 1;
-                Debug.Log(currentScore);
+                //score increases if employee is cheerful
+                if (employeeColor == Color.cyan)
+                {
+                    Debug.Log("Score is increasing based on productivity.");
+                    currentScore = currentScore + 0.07f;
+                    Debug.Log(currentScore);
+                }
+                
+                //score decreases if employee is annoyed
+                else if (employeeColor == Color.red)
+                {
+                    Debug.Log("Score is decreasing based on productivity.");
+                    currentScore = currentScore - 0.07f;
+                    Debug.Log(currentScore);
+                }
             }
         }
     }
