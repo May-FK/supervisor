142bc3d43f38e49c7f8649ccfd33038886bb9b8c
jenfield99 <64561971+jenfield99@users.noreply.github.com>
2020-12-08 23:55:27 -0500

    control screen fixes dw about it

---
+++ Supervisor/Assets/Scripts/UI Scripts/showcontrols.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showcontrols : MonoBehaviour
+{
+    public GameObject controls;
+
+    void Start()
+    {
+        controls.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Debug.Log("Escape key was pressed.");
+            ShowControls();
+        }
+        else
+        {
+            controls.SetActive(false);
+        }
+    }
+
+    public void ShowControls()
+    {
+        controls.SetActive(true);
+    }
+}
